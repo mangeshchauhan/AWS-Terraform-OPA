b068d44e3315c22787a690391706c6f8ef460172
mangeshchauhan <mangesh.225@gmail.com>
2025-09-26 17:31:12 +0530

    updating the workflow

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ module "sg" {
 module "ec2" {
   source         = "./modules/ec2"
   ami_id         = "ami-018046b953a698135" # Update with latest Amazon Linux or Ubuntu
-  instance_type  = "t2.large"
+  instance_type  = "t2.medium"
   subnet_id      = module.vpc.public_subnet_ids[0]
   sg_id          = module.sg.security_group_id
   key_name       = "mylinux" # Your EC2 KeyPair
